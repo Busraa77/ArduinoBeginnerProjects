eb5fee613a4cf5f72ac5327efca031982c05247b
Busraa77 <busraaozerr77@outlook.com>
2026-03-23 13:44:47 +0300

    Add files via upload

---
+++ 12_parlaklikKontrol/Code/parlaklkKontrol.ino
@@ -0,0 +1,17 @@
+int led = 9;
+int ayar = A0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(led,OUTPUT);
+
+}
+
+void loop() {
+  int deger=analogRead(ayar);
+  Serial.print("deger :");
+  Serial.println(deger);
+  int sonDeger = constrain(deger,0,1023);
+  int parlaklik = map(sonDeger,0,1023,0,255); // 0-2 saniye arasında bir bekleme süresi
+  analogWrite(led,parlaklik);
+}
